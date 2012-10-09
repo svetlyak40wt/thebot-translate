2d35f14497bad319d14aa18960ca561e37ac65fb
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-10-09 08:32:08 +0400

    Initial.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='thebot-translate',
+    version='0.1.0',
+    description='Uses translate.yandex.ru, to translate the texts.',
+    keywords='thebot language plugin',
+    license = 'New BSD License',
+    author="Alexander Artemenko",
+    author_email='svetlyak.40wt@gmail.com',
+    url='http://github.com/svetlyak40wt/thebot-translate/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Server',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    py_modules=['thebot_translate'],
+    install_requires=[
+        'thebot',
+        'requests',
+        'anyjson',
+    ],
+)
---
+++ thebot_translate.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+
+import thebot
+import anyjson
+import requests
+
+
+class Plugin(thebot.Plugin):
+    @thebot.route('translate (?P<some_text>.+)')
+    @thebot.route('translate (?P<some_text>.+) (?P<from_to>..-..)')
+    def on(self, request, some_text, from_to='en-ru'):
+        """Translates given text from one language to another.
+        """
+
+        if from_to not in self.get_directions():
+            request.respond(u'Supported translation directions are: ' + u', '.join(
+                sorted(list(self.get_directions()))
+            ))
+            return
+
+        response = requests.get(
+            'http://translate.yandex.net/api/v1/tr.json/translate',
+            params=dict(
+                lang=from_to,
+                text=some_text,
+            )
+        )
+        if response.status_code == 200:
+            data = anyjson.deserialize(response.content)
+            request.respond(u'\n'.join(data['text']))
+        else:
+            request.respond('Bad response from the server: {}, {}'.format(
+                response.status_code,
+                response.content
+            ))
+
+    def get_directions(self):
+        if not hasattr(self, '_directions'):
+            response = requests.get('http://translate.yandex.net/api/v1/tr.json/getLangs')
+            data = anyjson.deserialize(response.content)
+            self._directions = set(data['dirs'])
+
+        return self._directions
+
